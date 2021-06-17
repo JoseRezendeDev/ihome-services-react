df3149f2c21bc030215f143e4aa7a37f80637d50
Jose Rezende <zerezende12345@gmail.com>
2021-06-16 22:30:17 -0300

    Impl da chamada para o backend com fetch, mas com erro

--- src/Categoria.jsx
+++ src/Categoria.jsx
@@ -1,23 +1,25 @@
 import {
     Link
 } from 'react-router-dom';
-
-const categorias = [
-    {
-        id: 1,
-        nome: 'pedreiro'
-    },
-    {
-        id: 2,
-        nome: 'pintor'
-    },
-    {
-        id: 3,
-        nome: 'piscineiro'
-    }
-];
+import { useState, useEffect } from 'react';
 
 function Categoria(props) {
+    const [categorias, setCategorias] = useState([]);
+
+    useEffect(() => {
+        var myHeaders = new Headers();
+        const myInit = {
+            method: 'GET',
+            headers: myHeaders,
+            mode: 'no-cors',
+            cache: 'default'
+        };
+
+        fetch('http://localhost:80/categoria', myInit)
+            .then(resp => resp.json())
+            .then(data => setCategorias(data))
+    });
+
     return (
         <div>
             <h1>Categorias</h1>
